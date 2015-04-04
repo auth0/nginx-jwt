91f0e3c5d98c2ddfc9cc0d93d02e9ebc330668cc
Peter Stromquist <peter@twistedstream.com>
2015-04-04 13:54:18 -0500

    Rename "api" host to "backend"
    Since that's a more clear description of the role of that host

--- dev.sh
+++ dev.sh
@@ -6,15 +6,15 @@ NC='\033[0m' # No Color
 # get boot2docker host ip
 HOST_IP=$(boot2docker ip)
 
-echo "${cyan}Rebuilding the API image and restarting the API container...${NC}"
-docker rm -f api >/dev/null
-docker rmi -f api-image >/dev/null
-docker build -t="api-image" --force-rm hosts/api
-docker run --name api -d -p 5000:5000 api-image
+echo "${cyan}Rebuilding the backend image and restarting its container...${NC}"
+docker rm -f backend &>/dev/null
+docker rmi -f backend-image &>/dev/null
+docker build -t="backend-image" --force-rm hosts/backend
+docker run --name backend -d -p 5000:5000 backend-image
 
-echo "${cyan}Restarting the proxy container...${NC}"
-docker rm -f proxy >/dev/null
-docker run --name proxy -d -p 80:80 --add-host "api_host:$HOST_IP" -v "$PWD/hosts/proxy/nginx.conf":/etc/nginx/nginx.conf:ro nginx
+echo "${cyan}Restarting the proxy (Nginx) container...${NC}"
+docker rm -f proxy &>/dev/null
+docker run --name proxy -d -p 80:80 --add-host "backend_host:$HOST_IP" -v "$PWD/hosts/proxy/nginx.conf":/etc/nginx/nginx.conf:ro nginx
 
 echo "${cyan}Proxy:${NC}"
 echo curl http://$HOST_IP
