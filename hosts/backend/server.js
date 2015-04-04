d6d5664675d26f3f05242e376ff96e241eeef626
Peter Stromquist <peter@twistedstream.com>
2015-04-04 15:25:58 -0500

    Add request logging to backend service

--- hosts/backend/server.js
+++ hosts/backend/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
     res.send('Backend API root');
