a580bd7518e6d98a17c26f25520298fd2ec87c30
Peter Stromquist <peter@twistedstream.com>
2015-04-04 19:20:12 -0500

    Integration tests use jsonwebtoken instead of jws
    Which makes it easier to create a JWT from jws

--- test/test_integration.js
+++ test/test_integration.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var request = require('super-request');
-var jws = require('jws');
+var jwt = require('jsonwebtoken');
 
 var baseUrl = 'http://' + process.env.HOST_IP;
 var JWT_SECRET = 'g20id5X656tIYmbb3e4oIM4x1nI43947D87134uu5B';
@@ -35,15 +35,14 @@ describe('proxy endpoint', function () {
         });
 
         it("should return a 200 with the expected response header when a valid JWT is passed", function () {
-            var jwt = jws.sign({
-                header: { alg: 'HS256' },
-                payload: { sub: 'foo-user' },
-                secret: JWT_SECRET
-            });
+            var token = jwt.sign(
+                { sub: 'foo-user' },
+                JWT_SECRET
+            );
 
             return request(baseUrl)
                 .get('/secure')
-                .headers({'Authorization': 'Bearer ' + jwt})
+                .headers({'Authorization': 'Bearer ' + token})
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .expect({ message: 'This endpoint needs to be secure.' })
