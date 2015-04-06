d2a492e606e8fb2dfca54bf6509e611fbd337c07
Peter Stromquist <peter@twistedstream.com>
2015-04-06 04:39:53 -0500

    Use proper log level constants with ngx.log()

--- nginx-jwt.lua
+++ nginx-jwt.lua
@@ -1,18 +1,18 @@
 local auth_header = ngx.var.http_Authorization
 
 if auth_header == nil then
-    ngx.log(ngx.STDERR, "No Authorization header")
+    ngx.log(ngx.WARN, "No Authorization header")
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 else
-    ngx.log(ngx.STDERR, "Authorization: " .. auth_header)
+    ngx.log(ngx.INFO, "Authorization: " .. auth_header)
 
     local _, _, token = string.find(auth_header, "Bearer%s+(.+)")
 
     if token == nil then
-        ngx.log(ngx.STDERR, "Missing token")
+        ngx.log(ngx.WARN, "Missing token")
         ngx.exit(ngx.HTTP_UNAUTHORIZED)
     else
-        ngx.log(ngx.STDERR, "Token: " .. token)
+        ngx.log(ngx.INFO, "Token: " .. token)
         return
     end
 end
