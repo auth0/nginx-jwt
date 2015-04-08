14ba157da7c7a9771ef451dfb98560ab35e57aba
Peter Stromquist <peter@twistedstream.com>
2015-04-07 23:29:01 -0500

    Integration tests handle normal and base64 secrets

--- run.sh
+++ run.sh
@@ -6,10 +6,12 @@ NC='\033[0m' # No Color
 # get boot2docker host ip
 HOST_IP=$(boot2docker ip)
 
-echo "${cyan}Rebuilding the backend image and restarting its container...${NC}"
+echo "${cyan}Stopping the backend container and removing its image...${NC}"
 docker rm -f backend &>/dev/null
 docker rmi -f backend-image &>/dev/null
+echo "${cyan}Building a new backend image...${NC}"
 docker build -t="backend-image" --force-rm hosts/backend
+echo "${cyan}Starting a new backend image...${NC}"
 docker run --name backend -d -p 5000:5000 backend-image
 
 echo "${cyan}Prepping files for the proxy (Nginx) container...${NC}"
@@ -17,17 +19,28 @@ rm -rf lib
 curl https://codeload.github.com/SkyLothar/lua-resty-jwt/tar.gz/master | tar -xz --strip 1 lua-resty-jwt-master/lib
 curl https://codeload.github.com/jkeys089/lua-resty-hmac/tar.gz/master | tar -xz --strip 1 lua-resty-hmac-master/lib
 curl https://codeload.github.com/aiq/basexx/tar.gz/v0.1.0 | tar -xz --strip 1 basexx-0.1.0/lib
-rm -rf hosts/proxy/nginx/lua
-mkdir -p hosts/proxy/nginx/lua
-cp nginx-jwt.lua hosts/proxy/nginx/lua
-cp -r lib/resty hosts/proxy/nginx/lua
-cp -r lib/basexx.lua hosts/proxy/nginx/lua
+rm -rf hosts/proxy/normal-secret/nginx/lua
+rm -rf hosts/proxy/base64-secret/nginx/lua
+mkdir -p hosts/proxy/normal-secret/nginx/lua
+mkdir -p hosts/proxy/base64-secret/nginx/lua
+cp nginx-jwt.lua hosts/proxy/normal-secret/nginx/lua
+cp nginx-jwt.lua hosts/proxy/base64-secret/nginx/lua
+cp -r lib/resty hosts/proxy/normal-secret/nginx/lua
+cp -r lib/resty hosts/proxy/base64-secret/nginx/lua
+cp -r lib/basexx.lua hosts/proxy/normal-secret/nginx/lua
+cp -r lib/basexx.lua hosts/proxy/base64-secret/nginx/lua
 
-echo "${cyan}Restarting the proxy (Nginx) container...${NC}"
+echo "${cyan}Stopping the proxy (Nginx) containers and removing their images...${NC}"
 docker rm -f proxy &>/dev/null
+docker rm -f proxy-base64-secret &>/dev/null
 docker rmi -f proxy-image &>/dev/null
-docker build -t="proxy-image" --force-rm hosts/proxy
+docker rmi -f proxy-base64-secret-image &>/dev/null
+echo "${cyan}Building new proxy images...${NC}"
+docker build -t="proxy-image" --force-rm hosts/proxy/normal-secret
+docker build -t="proxy-base64-secret-image" --force-rm hosts/proxy/base64-secret
+echo "${cyan}Starting new proxy container...${NC}"
 docker run --name proxy -d -p 80:80 --add-host "backend_host:$HOST_IP" proxy-image
+docker run --name proxy-base64-secret -d -p 81:80 --add-host "backend_host:$HOST_IP" proxy-base64-secret-image
 
 echo "${cyan}Proxy:${NC}"
 echo curl http://$HOST_IP
--- test/test_integration.js
+++ test/test_integration.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+/* global describe, it, before */
 
 'use strict';
 
@@ -6,48 +6,80 @@ var request = require('super-request');
 var jwt = require('jsonwebtoken');
 
 var baseUrl = 'http://' + process.env.HOST_IP;
-var JWT_SECRET = "JWTs are the best!";
+var url, secret;
 
 describe('proxy', function () {
-    describe("GET /", function () {
-        it("should return 200 with expected proxied response", function () {
-            return request(baseUrl)
-                .get('/')
-                .expect(200, "Backend API root")
-                .end();
+    describe("configured with normal secret", function () {
+        before(function () {
+            url = baseUrl;
+            secret = 'JWTs are the best!';
+        });
+
+        describe("GET /", function () {
+            it("should return 200 with expected proxied response", function () {
+                return request(url)
+                    .get('/')
+                    .expect(200, "Backend API root")
+                    .end();
+            });
+        });
+
+        describe("GET /secure", function () {
+            it("should return 401 when passing no JWT", function () {
+                return request(url)
+                    .get('/secure')
+                    .expect(401)
+                    .end();
+            });
+
+            it("should return 401 when passing a bogus JWT", function () {
+                return request(url)
+                    .get('/secure')
+                    .headers({'Authorization': 'Bearer not-a-valid-jwt'})
+                    .expect(401)
+                    .end();
+            });
+
+            it("should return a 200 with the expected response header when a valid JWT is passed", function () {
+                var token = jwt.sign(
+                    { sub: 'foo-user' },
+                    secret
+                );
+
+                return request(url)
+                    .get('/secure')
+                    .headers({'Authorization': 'Bearer ' + token})
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .expect({ message: 'This endpoint needs to be secure.' })
+                    .expect('X-Auth-UserId', 'foo-user')
+                    .end();
+            });
         });
     });
 
-    describe("GET /secure", function () {
-        it("should return 401 when passing no JWT", function () {
-            return request(baseUrl)
-                .get('/secure')
-                .expect(401)
-                .end();
+    describe("configured with URL-safe base-64 encoded secret", function () {
+        before(function () {
+            url = baseUrl + ':81';
+            secret = 'This secret is stored base-64 encoded on the proxy host';
         });
 
-        it("should return 401 when passing a bogus JWT", function () {
-            return request(baseUrl)
-                .get('/secure')
-                .headers({'Authorization': 'Bearer not-a-valid-jwt'})
-                .expect(401)
-                .end();
-        });
+        describe("GET /secure", function () {
+            it("should return a 200 with the expected response header when a valid JWT is passed", function () {
+                var token = jwt.sign(
+                    { sub: 'foo-user' },
+                    secret
+                );
 
-        it("should return a 200 with the expected response header when a valid JWT is passed", function () {
-            var token = jwt.sign(
-                { sub: 'foo-user' },
-                JWT_SECRET
-            );
-
-            return request(baseUrl)
-                .get('/secure')
-                .headers({'Authorization': 'Bearer ' + token})
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .expect({ message: 'This endpoint needs to be secure.' })
-                .expect('X-Auth-UserId', 'foo-user')
-                .end();
+                return request(url)
+                    .get('/secure')
+                    .headers({'Authorization': 'Bearer ' + token})
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .expect({ message: 'This endpoint needs to be secure.' })
+                    .expect('X-Auth-UserId', 'foo-user')
+                    .end();
+            });
         });
     });
 });
