38e832a82d5c1cb3062d161df8a3d02f670cecfe
Peter Stromquist <peter@twistedstream.com>
2015-04-10 10:55:36 -0500

    Expose table_contains helper function

--- nginx-jwt.lua
+++ nginx-jwt.lua
@@ -57,4 +57,11 @@ function M.auth()
     end
 end
 
+function M.table_contains(table, item)
+    for key, value in pairs(table) do
+        if value == item then return true end
+    end
+    return false
+end
+
 return M
