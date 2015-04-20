137c9b0905d6bbf3bd0b92d6be0613f3374b37e7
Peter Stromquist <peter@twistedstream.com>
2015-04-20 08:37:44 -0500

    Break out run/stop_backend.sh scripts

---
+++ scripts/run_backend.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -o pipefail
+set -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+. $script_dir/common.sh
+sh $script_dir/stop_backend.sh
+
+echo "${cyan}Building backend image...${no_color}"
+docker build -t="backend-image" --force-rm $hosts_base_dir/backend
+echo "${cyan}Starting backend container...${no_color}"
+docker run --name backend -d backend-image
---
+++ scripts/stop_backend.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -o pipefail
+set -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+. $script_dir/common.sh
+
+echo "${cyan}Stopping the backend container and removing its image...${no_color}"
+docker rm -f backend &>/dev/null || true
+docker rmi -f backend-image &>/dev/null || true
