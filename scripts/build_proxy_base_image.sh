27582ffc571befd8672368310d38147ff2347abb
Peter Stromquist <peter@twistedstream.com>
2015-04-20 08:44:15 -0500

    Break out build_proxy_base_image.sh base image

---
+++ scripts/build_proxy_base_image.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -o pipefail
+set -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+. $script_dir/common.sh
+
+echo "${cyan}Building base proxy image, if necessary...${NC}"
+image_exists=$(docker images | grep "proxy-base-image") || true
+if [ -z "$image_exists" ]; then
+    echo "${blue}Building image${no_color}"
+    docker build -t="proxy-base-image" --force-rm $proxy_base_dir
+else
+    echo "${blue}Base image already exists${no_color}"
+fi
