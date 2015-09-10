7bbd61a35f2858fbdbcc0f2b598c1ba9f7387af0
twistedstream <peter@twistedstream.com>
2015-09-10 09:56:22 -0500

    Tweak cp command so it works on Ubuntu bash
    Fixes #29

--- scripts/run_proxy_container.sh
+++ scripts/run_proxy_container.sh
@@ -18,7 +18,7 @@ echo -e "${blue}Deploying Lua scripts and depedencies${no_color}"
 rm -rf $proxy_dir/nginx/lua
 mkdir -p $proxy_dir/nginx/lua
 cp $root_dir/nginx-jwt.lua $proxy_dir/nginx/lua
-cp -r lib/ $proxy_dir/nginx/lua
+cp -r lib/* $proxy_dir/nginx/lua
 
 echo -e "${blue}Building the new image${no_color}"
 docker build -t="proxy-$proxy_name-image" --force-rm $proxy_dir
